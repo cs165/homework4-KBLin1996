ae0e14aeb9a8cc058f6c277c182f55ab4657c94e
KB <popotwtw@gmail.com>
2019-05-19 16:44:18 +0800

    Update music-screen.js

--- music-screen.js
+++ music-screen.js
@@ -8,8 +8,56 @@
 //
 // See HW4 writeup for more hints and details.
 class MusicScreen {
-  constructor() {
+  constructor(containerElement) {
     // TODO(you): Implement the constructor and add fields as necessary.
+    this.containerElement = containerElement;
+    this.gifboxElement = containerElement.querySelector("#gifbox");
+    this.playElement = containerElement.querySelector("#play");
+    this.exitElement = containerElement.querySelector("#exit");
+    this.audioPlayer = null;
+    this.gifDisplay = null;
+    this.playButton = new PlayButton(this.playElement);
+    this.status = false;
+
+    this._Status = this._Status.bind(this);
+
+    document.addEventListener("Status", this._Status);
+    
+    this._onExit();
   }
   // TODO(you): Add methods as necessary.
+  preload(imgURL, songURL) {
+    this.gifDisplay = new GifDisplay(this.gifboxElement, imgURL);
+    this.audioPlayer = new AudioPlayer();
+    this.audioPlayer.setSong(songURL);
+    this.audioPlayer.setKickCallback( () => {
+      document.dispatchEvent(new CustomEvent("Kick"));
+    });
+    this.playButton.reset();
+  }
+  show() {
+    this.containerElement.classList.remove('inactive');
+  }
+
+  hide() {
+    this.containerElement.classList.add('inactive');
+    this.status = false;
+    this.audioPlayer.pause();
+  }
+
+  _onExit() {
+    this.exitElement.addEventListener('click', () => {
+      document.dispatchEvent(new CustomEvent("Exit"));
+    });
+  }
+
+  _Status() {
+    if(this.status) { //to pause
+      this.status = false;
+      this.audioPlayer.pause();
+    }else { //to play
+      this.status = true;
+      this.audioPlayer.play();
+    }
+  }
 }
