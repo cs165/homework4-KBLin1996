09206c5eb3808fbad680ec3fbc12833f17e739e1
KB <popotwtw@gmail.com>
2019-05-20 14:51:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,49 +5,63 @@
 class App {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
+    const menuElement = document.querySelector("#menu");
+    this.menuScreen = new MenuScreen(menuElement);
 
-      const menu = document.getElementById('menu');
-        this.tomusic = this.tomusic.bind(this);
-        document.addEventListener('keyword',this.tomusic);
+    this.loadingElement = document.querySelector("#loading");
+    
+    const musicElement = document.querySelector("#music");
+    this.musicScreen = new MusicScreen(musicElement);
 
-      const music = document.getElementById('music')
-        this.Gifenough = this.Gifenough.bind(this);
+    this.URL = "https://api.giphy.com/v1/gifs/search?q=";
+    this.LOADING = "Loading... ";
 
+    this._Fetching = this._Fetching.bind(this);
+    this._Loading = this._Loading.bind(this);
+    this._Loaded = this._Loaded.bind(this);
+    this._Exit = this._Exit.bind(this);
 
-      this.menuscreen = new MenuScreen(menu);
-      //this.menuscreen.hide();
-
-      this.music = new MusicScreen(music);
-      this.music.hide();
-
-      //this.Gif = new GifDisplay();
-
-
+    document.addEventListener("Fetching", this._Fetching);
+    document.addEventListener("Loading", this._Loading);
+    document.addEventListener("Loaded", this._Loaded);
+    document.addEventListener("Exit", this._Exit);
   }
   // TODO(you): Add methods as necessary.
-
-    tomusic(event)
-    {
-        this.music.Gif.GetGif(event.detail.Theme , this.Gifenough);
-    }
-
-    Gifenough()
-    {
-        console.log('enough');
-
-        if(this.music.Gif.enough === true)
-        {
-            this.menuscreen.hide();
-            this.music.show();
-
-            this.music.player.setSong(this.menuscreen.nowurl);
-            this.music.player.setKickCallback(this.music._onkick);
-            this.music.player.play();
+  _Fetching(event) { //Thanks for Nian's api_key :P
+    this.loadingElement.classList.remove('inactive');
+    const URL = this.URL + encodeURIComponent(event.detail.gifValue) + "&api_key=FjJaTP04iY5rAwcEASKET51wyx9VZ2V8&limit=25&rating=g";
+    const onJsonReady = (json) => {
+      let imgURL = [];
+      if(json.data.length > 2) {
+        for(let index in json.data) {
+          const imgurl = json.data[index].images.downsized.url;           
+          imgURL.push(imgurl);
         }
-        else
-        {
-            let noGif = document.querySelector('#error');
-            noGif.classList.remove('inactive');
-        }
-    }
+        this.menuScreen.hideErrMsg();
+        this.menuScreen.hide();
+        this.musicScreen.preload(imgURL, event.detail.songValue);
+      }else {
+        this.menuScreen.showErrMsg();
+      }
+    };
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(onJsonReady);
+  }
+
+  _Loaded() {
+    this.loadingElement.classList.add('inactive');
+    this.musicScreen.show();
+  }
+
+  _Loading(event) {
+    this.loadingElement.textContent = this.LOADING + event.detail.Index + '%';
+  }
+
+  _Exit() {
+    this.loadingElement.textContent = this.LOADING;
+    this.musicScreen.hide();
+    this.menuScreen.show();
+  }
 }
