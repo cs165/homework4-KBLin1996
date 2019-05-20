447ec462b72a97c6ffcbf7ecf1e1ee702c53e192
KB <popotwtw@gmail.com>
2019-05-20 14:33:41 +0800

    Update music-screen.js

--- music-screen.js
+++ music-screen.js
@@ -8,56 +8,53 @@
 //
 // See HW4 writeup for more hints and details.
 class MusicScreen {
-  constructor(containerElement) {
+  constructor(container, onCancel) {
     // TODO(you): Implement the constructor and add fields as necessary.
-    this.containerElement = containerElement;
-    this.gifboxElement = containerElement.querySelector("#gifbox");
-    this.playElement = containerElement.querySelector("#play");
-    this.exitElement = containerElement.querySelector("#exit");
-    this.audioPlayer = null;
-    this.gifDisplay = null;
-    this.playButton = new PlayButton(this.playElement);
-    this.status = false;
-
-    this._Status = this._Status.bind(this);
-
-    document.addEventListener("Status", this._Status);
-    
-    this._onExit();
+    this.container = container;
+    this.onCancel = onCancel;
+    this.gifContainer = container.querySelector('.gif');
+    this.btn = container.querySelector('.play-btn');
+    this.loading = container.querySelector('.loading');
+    this.playing = false;
   }
   // TODO(you): Add methods as necessary.
-  preload(imgURL, songURL) {
-    this.gifDisplay = new GifDisplay(this.gifboxElement, imgURL);
-    this.audioPlayer = new AudioPlayer();
-    this.audioPlayer.setSong(songURL);
-    this.audioPlayer.setKickCallback( () => {
-      document.dispatchEvent(new CustomEvent("Kick"));
-    });
-    this.playButton.reset();
-  }
-  show() {
-    this.containerElement.classList.remove('inactive');
-  }
+  play({ songValue, gifValue }) {
+    this._toggleHidden();
 
-  hide() {
-    this.containerElement.classList.add('inactive');
-    this.status = false;
-    this.audioPlayer.pause();
-  }
+    this.gif = new GifDisplay(this.gifContainer, gifValue, this._onSuccess.bind(this), this._onError.bind(this));
+    this.audio = new AudioPlayer();
+    this.playBtn = new PlayButton(this.btn, this._togglePlayer.bind(this));
 
-  _onExit() {
-    this.exitElement.addEventListener('click', () => {
-      document.dispatchEvent(new CustomEvent("Exit"));
+    this.audio.setSong(songValue);
+    this.audio.setKickCallback(() => {
+      console.log('Kick!');
+      this.gif.changeImage();
     });
   }
-
-  _Status() {
-    if(this.status) { //to pause
-      this.status = false;
-      this.audioPlayer.pause();
-    }else { //to play
-      this.status = true;
-      this.audioPlayer.play();
+  _toggleLoading() {
+    this.loading.classList.toggle('inactive');
+  }
+  _toggleHidden() {
+    this.container.classList.toggle('inactive');
+  }
+  _onSuccess() {
+    this._toggleLoading();
+    this._togglePlayer();
+  }
+  _onError() {
+    this._toggleHidden();
+    this.onCancel();
+  }
+  _togglePlayer() {
+    if (this.playing) {
+      this.playing = false;
+      this.audio.pause();
+      this.playBtn.play();
+    }
+    else {
+      this.playing = true;
+      this.audio.play();
+      this.playBtn.pause();
     }
   }
 }
