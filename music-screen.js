8c5dca4d2b6ec246a7e7901a82dc0778d50ac1e0
KB <popotwtw@gmail.com>
2019-05-20 14:53:29 +0800

    Update music-screen.js

--- music-screen.js
+++ music-screen.js
@@ -8,67 +8,56 @@
 //
 // See HW4 writeup for more hints and details.
 class MusicScreen {
-  constructor(element) {
+  constructor(containerElement) {
     // TODO(you): Implement the constructor and add fields as necessary.
-      this.element = element ;
-      //this.button = this.element.querySelector('.btn');
-      this.Gifscreen = this.element.querySelector('.screen');
+    this.containerElement = containerElement;
+    this.gifboxElement = containerElement.querySelector("#gifbox");
+    this.playElement = containerElement.querySelector("#play");
+    this.exitElement = containerElement.querySelector("#exit");
+    this.audioPlayer = null;
+    this.gifDisplay = null;
+    this.playButton = new PlayButton(this.playElement);
+    this.status = false;
 
-      this.player = new AudioPlayer();
-      this.Gif = new GifDisplay();
-      this.play = new PlayButton(this.player);
+    this._Status = this._Status.bind(this);
 
-      this.hide = this.hide.bind(this);
-      this.show = this.show.bind(this);
-      this._onkick = this._onkick.bind(this);
-      //this.Btnstate = this.Btnstate.bind(this);
-
-      //this.button.addEventListener('click',this.Btnstate);
-
-      //this.P = true ;
-
-
+    document.addEventListener("Status", this._Status);
+    
+    this._onExit();
   }
   // TODO(you): Add methods as necessary.
-
-  hide()
-  {
-    this.element.classList.add('inactive');
+  preload(imgURL, songURL) {
+    this.gifDisplay = new GifDisplay(this.gifboxElement, imgURL);
+    this.audioPlayer = new AudioPlayer();
+    this.audioPlayer.setSong(songURL);
+    this.audioPlayer.setKickCallback( () => {
+      document.dispatchEvent(new CustomEvent("Kick"));
+    });
+    this.playButton.reset();
+  }
+  show() {
+    this.containerElement.classList.remove('inactive');
   }
 
-  show()
-  {
-    this.element.classList.remove('inactive');
-/*
-    this.player.setSong();
-    this.player.setKickCallback();
-    this.player.play();
-    */
+  hide() {
+    this.containerElement.classList.add('inactive');
+    this.status = false;
+    this.audioPlayer.pause();
   }
 
-  _onkick()
-  {
-      console.log('kick!');
-      //this.Gif.ChangeGif();
+  _onExit() {
+    this.exitElement.addEventListener('click', () => {
+      document.dispatchEvent(new CustomEvent("Exit"));
+    });
   }
 
-  /*
-  Btnstate()
-  {
-      console.log('click');
-      if(this.P === false)
-      {
-          this.button.src = "images/pause.png" ;
-          this.player.play();
-          this.P = true ;
-      }
-      else
-      {
-          this.button.src = "images/play.png" ;
-          this.player.pause();
-          this.P = false ;
-      }
+  _Status() {
+    if(this.status) { //to pause
+      this.status = false;
+      this.audioPlayer.pause();
+    }else { //to play
+      this.status = true;
+      this.audioPlayer.play();
+    }
   }
-    */
-
 }
