48b4ec722d2d6afb703b5c8068bda56f3f5ba1c7
KB <popotwtw@gmail.com>
2019-05-20 14:35:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,63 +5,18 @@
 class App {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
-    const menuElement = document.querySelector("#menu");
-    this.menuScreen = new MenuScreen(menuElement);
+    const menu = document.getElementById('menu');
+    const player = document.getElementById('music-player');
 
-    this.loadingElement = document.querySelector("#loading");
-    
-    const musicElement = document.querySelector("#music");
-    this.musicScreen = new MusicScreen(musicElement);
-
-    this.URL = "https://api.giphy.com/v1/gifs/search?q=";
-    this.LOADING = "Loading... ";
-
-    this._Fetching = this._Fetching.bind(this);
-    this._Loading = this._Loading.bind(this);
-    this._Loaded = this._Loaded.bind(this);
-    this._Exit = this._Exit.bind(this);
-
-    document.addEventListener("Fetching", this._Fetching);
-    document.addEventListener("Loading", this._Loading);
-    document.addEventListener("Loaded", this._Loaded);
-    document.addEventListener("Exit", this._Exit);
+    this.menuScreen = new MenuScreen(menu, this._submut.bind(this));
+    this.musicScreen = new MusicScreen(player, this._cancel.bind(this));
   }
   // TODO(you): Add methods as necessary.
-  _Fetching(event) { //Thanks for Nian's api_key :P
-    this.loadingElement.classList.remove('inactive');
-    const URL = this.URL + encodeURIComponent(event.detail.gifValue) + "&api_key=FjJaTP04iY5rAwcEASKET51wyx9VZ2V8&limit=25&rating=g";
-    const onJsonReady = (json) => {
-      let imgURL = [];
-      if(json.data.length > 2) {
-        for(let index in json.data) {
-          const imgurl = json.data[index].images.downsized.url;           
-          imgURL.push(imgurl);
-        }
-        this.menuScreen.hideErrMsg();
-        this.menuScreen.hide();
-        this.musicScreen.preload(imgURL, event.detail.songValue);
-      }else {
-        this.menuScreen.showErrMsg();
-      }
-    };
-
-    fetch(URL)
-      .then(response => response.json())
-      .then(onJsonReady);
+  _submut(data) {
+    this.menuScreen.hide();
+    this.musicScreen.play(data);
   }
-
-  _Loaded() {
-    this.loadingElement.classList.add('inactive');
-    this.musicScreen.show();
-  }
-
-  _Loading(event) {
-    this.loadingElement.textContent = this.LOADING + event.detail.Index + '%';
-  }
-
-  _Exit() {
-    this.loadingElement.textContent = this.LOADING;
-    this.musicScreen.hide();
-    this.menuScreen.show();
+  _cancel() {
+    this.menuScreen.error();
   }
 }
