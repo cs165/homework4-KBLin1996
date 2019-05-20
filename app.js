590c29d93f4299cdce2d8cf1ec03fed997851c9d
KB <popotwtw@gmail.com>
2019-05-20 14:46:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,49 @@
 class App {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
-    const menu = document.getElementById('menu');
-    const player = document.getElementById('music-player');
 
-    this.menuScreen = new MenuScreen(menu, this._submut.bind(this));
-    this.musicScreen = new MusicScreen(player, this._cancel.bind(this));
+      const menu = document.getElementById('menu');
+        this.tomusic = this.tomusic.bind(this);
+        document.addEventListener('keyword',this.tomusic);
+
+      const music = document.getElementById('music')
+        this.Gifenough = this.Gifenough.bind(this);
+
+
+      this.menuscreen = new MenuScreen(menu);
+      //this.menuscreen.hide();
+
+      this.music = new MusicScreen(music);
+      this.music.hide();
+
+      //this.Gif = new GifDisplay();
+
+
   }
   // TODO(you): Add methods as necessary.
-  _submut(data) {
-    this.menuScreen.hide();
-    this.musicScreen.play(data);
-  }
-  _cancel() {
-    this.menuScreen.error();
-  }
+
+    tomusic(event)
+    {
+        this.music.Gif.GetGif(event.detail.Theme , this.Gifenough);
+    }
+
+    Gifenough()
+    {
+        console.log('enough');
+
+        if(this.music.Gif.enough === true)
+        {
+            this.menuscreen.hide();
+            this.music.show();
+
+            this.music.player.setSong(this.menuscreen.nowurl);
+            this.music.player.setKickCallback(this.music._onkick);
+            this.music.player.play();
+        }
+        else
+        {
+            let noGif = document.querySelector('#error');
+            noGif.classList.remove('inactive');
+        }
+    }
 }
